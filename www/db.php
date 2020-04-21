1df2999b2256b93e3c4ea552d33392ece634d9c8
yunitanc <64052044+yunitanc@users.noreply.github.com>
2020-04-21 09:14:29 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id9420412_tugas81","dondo","id9420412_tugas8") or die ("could not connect database");
+ $con = mysqli_connect("localhost","root","","") or die ("could not connect database");
 ?>
